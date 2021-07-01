1510ca1804f7c1490477a1e37a370b8b777cd456
Teodora.Toncheva <tedina@gmail.com>
2021-07-01 07:10:38 +0300

    Initial commit

---
+++ src/main/java/takeaway/divider/DividerApplication.java
@@ -0,0 +1,13 @@
+package takeaway.divider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividerApplication.class, args);
+	}
+
+}
